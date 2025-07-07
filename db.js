56f5e96bd31e5aab61d4a85ef5f97653599a8128
akratimaheshwari <akratimaheshwari634@gmail.in>
2025-07-07 14:29:29 +0530

    change localhost to cloud link

--- db.js
+++ db.js
@@ -1,17 +1,21 @@
-import mongoose from 'mongoose';
+import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 dotenv.config()
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected');
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    console.log('MongoDB connected')
   } catch (err) {
-    console.error('DB connection failed:', err.message);
-    process.exit(1); // Stop the app on DB error
+    console.error('DB connection failed:', err.message)
+    process.exit(1)
   }
-};
+}
 
-export default connectDB;
+export default connectDB
+
 
 
