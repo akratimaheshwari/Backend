4a8d36bebb3cfa83048ebf8169d01971693731af
lakshrajput_22 <lalkeshrajputofficial@gmail.com>
2025-10-28 15:29:07 +0530

    change not found page

--- rentkart-client/src/pages/NotFound.jsx
+++ rentkart-client/src/pages/NotFound.jsx
@@ -36,7 +36,7 @@ const NotFound = () => {
           className="inline-flex items-center gap-2 bg-gray-800 text-white px-6 py-3 rounded-full hover:bg-gray-900 transition"
         >
           <ArrowLeft size={18} />
-          Go Back Home
+          404 404
         </button>
       </main>
     </div>
