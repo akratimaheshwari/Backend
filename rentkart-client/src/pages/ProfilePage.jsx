fadb738905ca91d305a93f74e0da013a0e2761f4
lakshrajput_22 <lalkeshrajputofficial@gmail.com>
2025-10-28 15:30:37 +0530

    Change profile Page

--- rentkart-client/src/pages/ProfilePage.jsx
+++ rentkart-client/src/pages/ProfilePage.jsx
@@ -107,7 +107,7 @@ const ProfilePage = () => {
                             className="inline-flex items-center px-3 py-2 border border-red-300 rounded-md text-sm font-medium text-red-600 bg-white hover:bg-red-50 transition-colors"
                         >
                             <LogOut className="w-4 h-4 mr-2" />
-                            Logout
+                            LogIn
                         </button>
                     </div>
                 </div>
